ee2be692c99d9b52bd50b479628dff2323d281dd
NYU Networks Group <jiawu17@gmail.com>
2018-02-01 15:49:56 -0500

    emulate_house complete, automatically configures network

--- util/emulate_house.sh
+++ util/emulate_house.sh
@@ -2,7 +2,13 @@
 # Run it as: bash emulate_house.sh
 
 # Remove existing configurations
-sudo tc qdisc del dev enp3s0f1 root
-sudo tc qdisc del dev enx000ec688fc5c root
+sudo tc qdisc del dev enp3s0f1 root > /dev/null 2>&1
+sudo tc qdisc del dev enx000ec688fc5c root > /dev/null 2>&1
 
 # Prints and runs configurations
+OUTPUT="$(python -W ignore src/digitaldivideutil.py --netem-down --netem-up --json)"
+UP="$(printf "%s\n" "${OUTPUT}" | head -n -2 | tail -1)"
+DOWN="$(printf "%s\n" "${OUTPUT}" | head -n -1 | tail -1)"
+eval $UP
+eval $DOWN
+printf "%s\n" "${OUTPUT}"
